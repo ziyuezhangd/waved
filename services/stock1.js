21a8b708b46e67b1393e695012044e37bbb59c88
advaNoda <2020192040@email.szu.edu.cn>
2025-07-29 16:54:37 +0800

    1

--- services/stock1.js
+++ services/stock1.js
@@ -30,18 +30,18 @@ import yahooFinance from 'yahoo-finance2';
 export async function getIntradayData(symbol, period = '1d', interval = '1m') {
   if (period === '1d') {
     return {
-      xAxis: ['09:30', '09:31', '09:32'],
-      series: [123.45, 123.55, 123.65]
+      times: ['09:30', '09:31', '09:32'],
+      prices: [123.45, 123.55, 123.65]
     };
   } else if (period === '5d') {
     return {
-      xAxis: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-      series: [120, 121, 122, 123, 124]
+      times: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+      prices: [120, 121, 122, 123, 124]
     };
   } else if (period === '1mo') {
     return {
-      xAxis: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-      series: [110, 115, 120, 125]
+      times: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+      prices: [110, 115, 120, 125]
     };
   }
   return { times: [], prices: [] };
