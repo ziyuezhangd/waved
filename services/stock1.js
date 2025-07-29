00ac16068df0d35df07bcf716c15952823c5f899
advaNoda <2020192040@email.szu.edu.cn>
2025-07-29 16:17:02 +0800

    solve 500 first try

--- services/stock1.js
+++ services/stock1.js
@@ -5,7 +5,7 @@ export async function getStockData(symbol) {
   return quote.regularMarketPrice;
 }
 
-export async function getIntradayData(symbol) {
+/* export async function getIntradayData(symbol) {
   const today = new Date();
   const start = new Date(today.getFullYear(), today.getMonth(), today.getDate(),9, 30);
   const end = new Date(today.getFullYear(), today.getMonth(), today.getDate(),16, 0);
@@ -22,5 +22,14 @@ export async function getIntradayData(symbol) {
   return {
     times: result.timestamp.map(ts => new Date(ts * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })),
     prices: result.indicators.quote[0].close,
+  };
+
+
+}; */
+//temporarily mock
+export async function getIntradayData(symbol) {
+  return {
+    times: ['09:30', '09:35', '09:40'],
+    prices: [123.45, 123.55, 123.65]
   };
 }
